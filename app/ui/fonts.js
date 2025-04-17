3c6ee18a720f9985ecc7d7293918dae94fe1bfc4
Jeremygim2002 <127067985+Jeremygim2002@users.noreply.github.com>
2025-04-17 16:38:18 -0500

    correcion de errores

--- app/ui/fonts.js
+++ app/ui/fonts.js
@@ -2,14 +2,17 @@ import {Quicksand, Roboto, Lusitana} from 'next/font/google';
 
 export const quicksand = Quicksand({
     subset: ['latin'],
+    preload: true,
 })
 
 export const roboto = Roboto({
     subset: ['latin'],
-    weight: ['400', '500', '700']
+    weight: ['400'],
+    preload: true,
 })
 
 export const lusitana = Lusitana({
     subset: ['latin'],
-    weight: ['400']
+    weight: ['400'],
+    preload: true,
 })
